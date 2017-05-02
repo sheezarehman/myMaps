60e22f82eddecf569c60ad4deda7cb9f0011a64c
sheeza <sheeza.rehman@epitech.eu>
2017-05-02 18:37:23 +0200

    general or not

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html>
   <head>
-   <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
     <h3>My Google Maps Demo</h3>
+    <div class="right">
+        <form action="" method="GET" class="form-inline">
+            <div class="form-group">
+                <label for="start">Départ:</label>
+                <input type="text" name="start" class="form-control" id="start">
+                <label for="start2"><input type="checkbox">current location</label>
+            </div>
+            <br>
+            <div class="form-group">
+                <label for="end">Arrivée:</label>
+                <input type="text" name="end" class="form-control" id="end">
+                <label for="end2"><input type="checkbox">current location</label>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
     <div id="map"></div>
     <script src="js/javascript.js"></script>
     <script async defer
--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,74 @@
 console.log("tada");
 
 function initMap() {
-	var uluru = {lat: 48.85 , lng: 2.3};
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 4,
-	  center: uluru,
-	  fullscreenControl: true
-	});
-	var marker = new google.maps.Marker({
-	  position: uluru,
-	  map: map
-	});
+    if (urlParam('start') != '') {  // variable_name would be the name of your variable within your url following the ? symbol
+
+        var uluru = {lat: 48.85 , lng: 2.3};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru,
+            fullscreenControl: true
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    } else {//general
+
+        general();
+    }
 }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+var map, infoWindow;
+function general() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function urlParam(name){
+    if(new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href) !== null ){
+        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        return  results[1];
+    }
+    return false;
+    // return true;
+}
+window.onload = function() {
+
+    if (urlParam('start') != '') {  // variable_name would be the name of your variable within your url following the ? symbol
+        //
+    } else {
+        general();
+    }
+};
